df27847f873af3e3da593409cf96a8a53965df4c
Lucas Praxedes <luk_288@hotmail.com>
2022-12-27 15:01:49 -0300

    feat(errors):Error Handling

--- dist/main.js
+++ dist/main.js
@@ -3,10 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("@nestjs/common");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const errors_interceptor_1 = require("./error/errors.interceptor");
+const http_exception_filter_1 = require("./error/http-exception.filter");
 require('../patch.js');
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe());
+    app.useGlobalFilters(new http_exception_filter_1.HttpExceptionFilter());
+    app.useGlobalInterceptors(new errors_interceptor_1.ErrorsInterceptor());
     await app.listen(3000);
 }
 bootstrap();
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ErrorsInterceptor } from './error/errors.interceptor';
+import { HttpExceptionFilter } from './error/http-exception.filter';
 
 require('../patch.js');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ErrorsInterceptor());
   await app.listen(3000);
 }
 bootstrap();
